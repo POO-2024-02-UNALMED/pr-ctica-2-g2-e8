3ff38cc3870e9355d13de7b992f90f51452fb9bd
OkarRojas <orojasb@unal.edu.co>
2025-02-25 12:43:35 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,6 +3,7 @@ python -m venv env_name
 call env_name\Scripts\activate
 pip install -r requirements.txt
 pip freeze > requirements.txt
-python C:\Users\oscar\OneDrive\Escritorio\POO\pr-ctica-2-g2-e8\src\main.py
+cd src
+python main.py
 @echo off
 pause
