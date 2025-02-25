441c685120dc1c163b52b8cf07f0d674513cb465
OkarRojas <orojasb@unal.edu.co>
2025-02-25 12:40:28 -0500

    Update

---
+++ run.bat
@@ -0,0 +1,8 @@
+
+python -m venv env_name
+call env_name\Scripts\activate
+pip install -r requirements.txt
+pip freeze > requirements.txt
+python C:\Users\oscar\OneDrive\Escritorio\POO\pr-ctica-2-g2-e8\src\main.py
+@echo off
+pause
